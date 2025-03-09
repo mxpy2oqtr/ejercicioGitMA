55a828eebaddd2e4deb567757ed5ed339580bf6c
mxpy2oqtr <dulcekoko@gmail.com>
2025-03-09 19:44:52 +0100

    Inicio del proyecto: mensaje simulado

---
+++ script/virus.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# SIMULACIÓN DE VIRUS INOFENSIVO - PRÁCTICA EDUCATIVA
+yad --title="¡ALERTA! 🚨" \
+    --text="<span size='x-large' color='#FF0000'><b>¡TU SISTEMA ESTÁ INFECTADO!</b></span>\n\nEste es un ejercicio académico.\nNo hay daño real." \
+    --button="Cerrar:0" \
+    --width=400 \
+    --center
